b301989375208da8f94921d38055b3cf75fd57c9
macfisher <macfisher01@gmail.com>
2019-04-14 14:41:47 -0600

    Re-created project w/ generic "Samples" name

--- Samples/Samples.cpp
+++ Samples/Samples.cpp
@@ -1,4 +1,4 @@
-// Cherno.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Samples.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
