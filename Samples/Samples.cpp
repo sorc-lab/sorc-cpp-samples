9eb2ed1869384b913d0b608eb19dcd658869fe2a
macfisher <macfisher01@gmail.com>
2019-04-14 15:40:38 -0600

    Extensions and installation docs in comments, for now

--- Samples/Samples.cpp
+++ Samples/Samples.cpp
@@ -1,16 +1,20 @@
 // IDE font set to Liberation Mono, bold weight, 10pt size
 
 // Git Diff Margin tools that work like IntelliJ Git integration
-// Extensions->Manage Extensions
+	// Extensions->Manage Extensions
 
 // Remove the default "Indicator Margin" in Tools->Options->Text Editor->General
-	// Untick ""Indicator margin" and "Selection margin"
+	// Untick "Indicator margin" and "Selection margin"
+
+// Add a Column Indicator at column 120
+	// Download and enable "Editor Guidlines" extension
+	// Add guideline via right-click->Guidelines->Add Guideline (adds where cursor is)
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
