48e1288f57dff6c4fc510a8342c53c75e7f976f1
macfisher <macfisher01@gmail.com>
2019-04-14 15:24:37 -0600

    Commit for testing GitDiffMargin VS extension

--- Samples/Samples.cpp
+++ Samples/Samples.cpp
@@ -1,20 +1,17 @@
-// Samples.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// IDE font set to Liberation Mono, bold weight, 10pt size
+
+// Git Diff Margin tools that work like IntelliJ Git integration
+// Extensions->Manage Extensions
+
+// Remove the default "Indicator Margin" in Tools->Options->Text Editor->General
+	// Untick ""Indicator margin" and "Selection margin"
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    std::cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
