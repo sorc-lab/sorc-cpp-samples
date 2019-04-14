6bd45e267d14e5d2ebd9d4b81abee800d377a970
macfisher <macfisher01@gmail.com>
2019-04-14 16:18:31 -0600

    Finished tmp docs in main

--- Samples/Samples.cpp
+++ Samples/Samples.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+/** IDE SETTINGS: VISUAL STUDIO COMMUNITY v16 */
 // IDE font set to Liberation Mono, bold weight, 10pt size
+	// Consolas can be used if Mono fonts are not available
 
 // Git Diff Margin tools that work like IntelliJ Git integration
 	// Extensions->Manage Extensions
@@ -9,13 +21,3 @@
 // Add a Column Indicator at column 120
 	// Download and enable "Editor Guidlines" extension
 	// Add guideline via right-click->Guidelines->Add Guideline (adds where cursor is)
-
-#include <iostream>
-
-int main()
-{
-	std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
